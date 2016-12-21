13313df46dee3ed671c87fb79ea8e7907a8f6e5b
vubinhne <vubinhcht@gmail.com>
2016-12-21 17:54:10 +0700

    updates

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
 //    env = process.env.NODE_ENV ||
     env = 'development';
-
+1234
 var config = {
   development: {
     root: rootPath,
